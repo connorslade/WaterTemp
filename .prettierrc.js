84a309f10627d9f64b037aab9c72444b8d9aada6
Connor Slade <connor@connorcode.com>
2021-07-03 16:36:04 -0400

    🔥 Code Formatting Setup

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+    jsxBracketSameLine: false,
+    trailingComma: 'none',
+    arrowParens: 'avoid',
+    bracketSpacing: true,
+    singleQuote: true,
+    endOfLine: 'auto',
+    tabWidth: 4,
+    semi: false,
+    semi: true
+};
