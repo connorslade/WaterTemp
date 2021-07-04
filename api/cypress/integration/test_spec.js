1b00da5c2d49db37fe46da1f86d821dada4c0c7e
Connor Slade <connor@connorcode.com>
2021-07-03 21:08:34 -0400

    🧪 Testing
    Setup Cypress for UI testing

---
+++ api/cypress/integration/test_spec.js
@@ -0,0 +1,57 @@
+describe('Basic', () => {
+    it('Visits hosted web page', () => {
+        cy.wait(500)
+        cy.visit('http://localhost:8080')
+    })
+  })
+
+// Check data is comming from API
+describe('API Check', () => {
+  it('Temperature Data', () => {
+    cy.visit('http://localhost:8080')
+    cy.get('#temp').should(e => {
+        expect(e.text()).not.to.include('...')
+      })
+  })
+
+    it('Avg Temperature', () => {
+      cy.visit('http://localhost:8080')
+      cy.get('#avg').should(e => {
+        expect(e.text()).not.to.include('...')
+      })
+  })
+})
+
+describe('Unit Changing', () => {
+    it('Check unit set to F', () => {
+        cy.visit('http://localhost:8080')
+        cy.get('#unit').should('have.text', '°F')
+    })
+
+
+    it('Switch Unit to C', () => {
+        cy.visit('http://localhost:8080')
+        cy.get('#unit').click()
+        cy.get('#unit').should('have.text', '°C')
+    })
+
+    it('Swich Unit to K', () => {
+        cy.visit('http://localhost:8080')
+        cy.get('#unit').click()
+        cy.get('#unit').click()
+        cy.get('#unit').should('have.text', '°K')
+    })
+})
+
+describe('Chart', () => {
+    it('Check chart is invisible', () => {
+        cy.visit('http://localhost:8080')
+        cy.get('#graph').should('have.css', 'display', 'none')
+    })
+
+    it('Toggle Chart Visibility', () => {
+        cy.visit('http://localhost:8080')
+        cy.get('#graphToggle').click()
+        cy.get('#graph').should('have.css', 'display', 'block')
+    })
+})
