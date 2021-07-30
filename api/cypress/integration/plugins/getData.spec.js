a446fb33c196360f2657f499526d8a388250548d
Connor Slade <connor@connorcode.com>
2021-07-30 12:45:46 -0400

    🌳 More Data Get UI Tests

--- api/cypress/integration/plugins/getData.spec.js
+++ api/cypress/integration/plugins/getData.spec.js
@@ -35,3 +35,54 @@ describe('Data Download', () => {
         cy.get('#JSON_DOWNLOAD').click();
     });
 });
+
+describe('Stats data', () => {
+    it('Data Points', () => {
+        cy.visit('http://localhost:8080/data');
+        cy.get('#POINTS').should(e =>
+            expect(e.text()).not.to.include('{DATA_POINTS}')
+        );
+    });
+
+    it('Data Rate', () => {
+        cy.visit('http://localhost:8080/data');
+        cy.get('#RATE').should(e =>
+            expect(e.text()).not.to.include('{DATA_RATE}')
+        );
+    });
+
+    it('Data Min', () => {
+        cy.visit('http://localhost:8080/data');
+        cy.get('#MIN').should(e =>
+            expect(e.text()).not.to.include('{DATA_MIN}')
+        );
+    });
+
+    it('Data Max', () => {
+        cy.visit('http://localhost:8080/data');
+        cy.get('#MAX').should(e =>
+            expect(e.text()).not.to.include('{DATA_Max}')
+        );
+    });
+
+    it('Data Start', () => {
+        cy.visit('http://localhost:8080/data');
+        cy.get('#START').should(e =>
+            expect(e.text()).not.to.include('{DATA_S}')
+        );
+    });
+
+    it('Data End', () => {
+        cy.visit('http://localhost:8080/data');
+        cy.get('#END').should(e =>
+            expect(e.text()).not.to.include('{DATA_E}')
+        );
+    });
+
+    it('Data Size', () => {
+        cy.visit('http://localhost:8080/data');
+        cy.get('#SIZE').should(e =>
+            expect(e.text()).not.to.include('{DATA_SIZE}')
+        );
+    });
+});
