93eb44577230b5936fe3ff946844d61e9b859828
Connor Slade <connor@connorcode.com>
2021-07-31 23:13:55 -0400

    🧪 Add test for Error 404 Page

--- api/cypress/integration/main.spec.js
+++ api/cypress/integration/main.spec.js
@@ -121,3 +121,13 @@ describe('Chart', () => {
             });
     });
 });
+
+describe('Error 404 Page', () => {
+    it('Check 404 Page', () => {
+        cy.visit('http://localhost:8080/asdf', { failOnStatusCode: false });
+        cy.title().should('eq', 'Error');
+        cy.get('p')
+            .contains(/The page .* was not found... sorwy./)
+            .should(e => expect(e.text()).include('/asdf'));
+    });
+});
